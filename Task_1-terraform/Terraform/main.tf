8f9114e8ca2a6cf74f291fd3657333fc8f35beed
Rajath rao <128474801+rajath-infra-labs@users.noreply.github.com>
2026-02-09 17:39:47 +0530

    createing vm(debian) in gcp and Add Terraform configuration for Google Compute Engine
    Define Terraform configuration for Google Cloud provider and create a compute instance.

---
+++ Task_1-terraform/Terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_instance" "vm" {
+  name         = "tf-vm-1"
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
